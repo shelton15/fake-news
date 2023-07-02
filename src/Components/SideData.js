c3e11b8e5953b3344261afde56c3e9b52682f0e3
shelton15 <sheltoniyassa@gmail.com>
2023-07-02 18:05:37 +0100

    ...

--- src/Components/SideData.js
+++ src/Components/SideData.js
@@ -1,9 +1,23 @@
 import React from "react";
+import '../App.css';
+import HomeIcon from '@mui/icons-material/Home';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const SideData = [
     {
         title: "Home",
-        icon: <Icon />,
+        icon: <HomeIcon />,
         link: "/home"
+    },
+    {
+        title: "Dashboard",
+        icon: <DashboardIcon />,
+        link: "/dashboard"
+    },
+    {
+        title: "Logout",
+        icon: <LogoutIcon />,
+        link: "/logout"
     }
 ]
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,10 +1,26 @@
 import React from "react";
 import '../App.css';
+import {SideData} from './SideData';
 
 function Sidebar() {
     return (
         <div className="Sidebar">
-        Sidebar
+            <ul className="SidebarList">{SideData.map((val, key)=> {
+                return <li  
+                        key={key} 
+                        className="row"
+                        id={window.location.pathmane === val.link ? "active" : ""}
+                        onClick={()=> {
+                            window.location.pathname = val.link}
+                            }> 
+
+                            <div id="icon"> {val.icon}</div>
+                            <div id="title">{val.title}</div>
+
+                        </li>;
+            }
+            )}
+            </ul>
         </div>
     )
 }
