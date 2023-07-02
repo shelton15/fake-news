68278f911a633e2a4bbe731ab2e995e591df31f6
shelton15 <sheltoniyassa@gmail.com>
2023-07-02 16:19:30 +0100

    ...

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css';
+
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+        Sidebar
+        </div>
+    )
+}
+
+export default Sidebar
