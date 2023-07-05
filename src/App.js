b9cd466eee5e705a47c8dcb980ba52e129a25dad
shelton15 <sheltoniyassa@gmail.com>
2023-07-05 18:23:52 +0100

    routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Sidenav from "./Components/Sidenav";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Settings from "./Pages/Settings";
 
 
 export default function App() {
   return (
-    <div>
-      <Sidenav/>
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home/>}></Route>
+        <Route path="/about" exact element={<About/>}></Route>
+        <Route path="/settings" exact element={<Settings/>}></Route>
+      </Routes>
+      </BrowserRouter>
+    </>
   )
 }
