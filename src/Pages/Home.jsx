fbb015ec5976668a672515f4b2c97c41cd70f7c1
shelton15 <sheltoniyassa@gmail.com>
2023-07-07 07:39:14 +0100

    dependencies added and home page ui

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,9 +3,25 @@ import Navbar from "../Components/Navbar";
 import Sidenav from "../Components/Sidenav";
 import { Box } from "@mui/material";
 // import Typography from '@mui/material/Typography';
+import {useState, useEffect} from "react";
+import { useDropzone } from "react-dropzone";
 
 
 export default function Home() {
+    const [files, setFiles] = useState([]);
+    const {
+        isDragOver,
+        isDropped,
+        getRootProps,
+        open,
+        close,
+    } = useDropzone({
+        accept:".png, .jpg, .jpeg",
+        maxFiles:3,
+    });
+    useEffect(() =>{
+        if(isDropped){
+    })
     return (
         <>
         <Navbar/>
