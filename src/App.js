ba7856a45f911f58a604e2bfe5326ed21268ef5c
shelton15 <sheltoniyassa@gmail.com>
2023-07-14 17:36:05 +0100

    //

--- src/App.js
+++ src/App.js
@@ -7,17 +7,61 @@ import Settings from "./Pages/Settings";
 import  LoginForm  from "./Pages/LoginForm";
 import Register from "./Pages/Register";
 import { AuthProvider } from "./context/AuthProvider";
+import { AuthContext } from "./context/AuthProvider";
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { Dashboard } from "@mui/icons-material";
 
 export default function App() {
+  const { loading, user } = useContext(AuthContext);
   return (
     <AuthProvider>
       <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Home/>}></Route>
-        <Route path="/dashboard" exact element={<About/>}></Route>
-        <Route path="/settings" exact element={<Settings/>}></Route>
         <Route path="/login" exact element={<LoginForm/>}></Route>
         <Route path="/register" exact element={<Register/>}></Route>
+        <Route
+          path="/"
+          element={
+            loading ? (
+              <div className="flex h-[100vh] justify-center items-center">
+                {/* <Loader /> */}
+              </div>
+            ) : user ? (
+              <Home />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        ></Route>
+        <Route
+          path="/dashboard"
+          element={
+            loading ? (
+              <div className="flex h-[100vh] justify-center items-center">
+                {/* <Loader /> */}
+              </div>
+            ) : user ? (
+              <Dashboard />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        ></Route>
+        <Route
+          path="/settings"
+          element={
+            loading ? (
+              <div className="flex h-[100vh] justify-center items-center">
+                {/* <Loader /> */}
+              </div>
+            ) : user ? (
+              <Dashboard />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        ></Route>
       </Routes>
       </BrowserRouter>
     </AuthProvider>
