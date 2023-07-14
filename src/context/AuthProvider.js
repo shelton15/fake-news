8fbaccd6afe014e0873029e85f92fb183aedfc5a
shelton15 <sheltoniyassa@gmail.com>
2023-07-14 17:43:25 +0100

    //

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -8,7 +8,7 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const navigate = useNavigate;
+  const navigate = useNavigate();
 
   useEffect(() => {
     setLoading(true);
