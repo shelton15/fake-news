a9a1e800a9a9b29468c2677eeaa791fb1bd9ea22
shelton15 <sheltoniyassa@gmail.com>
2023-07-14 15:20:35 +0100

    login page added

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+const RegisterForm = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send POST request to register endpoint with username, email, and password data
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send POST request to login endpoint with email and password data
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
---
+++ src/context/AuthProvider.js
@@ -0,0 +1,115 @@
+/* eslint-disable no-unused-vars */
+import React, { createContext, useState, useEffect } from "react";
+import { base_url } from "../config";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    console.log(isLoggedIn);
+    if (isLoggedIn) {
+      getUser();
+    }
+  }, []);
+
+  const register = async (username, email, password) => {
+    const registerUrl = `${base_url}/users/register`;
+
+    const data = {
+      username: username,
+      email: email,
+      password: password,
+    };
+
+    fetch(registerUrl, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  };
+
+  const login = async (email, password) => {
+    console.log(`Logging in with --==> ${base_url}/users/login`);
+    try {
+      setLoading(true);
+      fetch(`${base_url}/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          const userInfo = res.data;
+          const token = res.token;
+          setUser(userInfo);
+          localStorage.setItem("token", token);
+          localStorage.setItem("isLoggedIn", true);
+          localStorage.setItem("id", userInfo.id);
+          setLoading(false);
+          console.log(userInfo);
+        })
+
+        .catch((e) => {
+          console.log(`Login error ${e}`);
+          setLoading(false);
+          setError(e);
+        });
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("id");
+    setUser(null);
+  };
+
+  const getUser = async () => {
+    console.log("fetching user");
+    setLoading(true);
+    const id = localStorage.getItem("id");
+    const token = localStorage.getItem("token");
+    try {
+      fetch(`${base_url}/users/${id}`, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setUser(data.company);
+          console.log(data.company);
+        });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, loading, error, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
